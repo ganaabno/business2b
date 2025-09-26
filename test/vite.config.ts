d88e89db2896e0a8afc264c64b129ca8f591b456
ganaabno <gg733328@gmail.com>
2025-09-26 15:10:21 +0800

    Routes

--- test/vite.config.ts
+++ test/vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/', // 👈 very important for SPA routing
+  build: {
+    outDir: 'dist', // 👈 make sure output is dist
+  },
 })
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-// https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
+    base: '/', // 👈 very important for SPA routing
+    build: {
+        outDir: 'dist', // 👈 make sure output is dist
+    },
 });
